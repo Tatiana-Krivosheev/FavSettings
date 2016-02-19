bf6b176d1b8e7c75ea1c1a6d6d33010ff92a247b
Tatiana Krivosheev <tatiana_krivosheev@yahoo.com>
2016-02-19 10:06:39 -0800

    Added instructions to make aliases in Windows

--- aliases.cmd
+++ aliases.cmd
@@ -1,5 +1,9 @@
 @echo off
 
+rem save this file as %USERPROFILE%\aliases.cmd
+rem run regedit and go to HKEY_CURRENT_USER -> Software -> Microsoft -> Command Processor
+rem add String Value entry with the name AutoRun and the full path of your .cmd file, %USERPROFILE%\aliases.cmd
+
 DOSKEY ls=dir $*
 DOSKEY ll=dir $*
 DOSKEY cp=copy $*
